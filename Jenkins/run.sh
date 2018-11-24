173e0be0f27ab6923a54048400b92713a0ce69f8
DenDen047 <sh.mn.nat@gmail.com>
2018-11-24 10:00:14 +0900

    ./run.shが動いた

--- Jenkins/run.sh
+++ Jenkins/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run \
+docker run --rm \
     -p 8080:8080 \
     -p 50000:50000 \
-    jenkins/jenkins:lts
+    -v `pwd`/jenkins_home:/var/jenkins_home \
+    denden047/jenkins
