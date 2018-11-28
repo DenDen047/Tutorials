d4c6a22f1d8b41c47b540daa0a674b611361fa9b
Unknown <sh.mn.nat@gmail.com>
2018-11-28 11:50:01 +0900

    fix

--- mpi4py/run.sh
+++ mpi4py/run.sh
@@ -3,8 +3,6 @@
 CMD="mpiexec --allow-run-as-root -np 2 python main.py"
 
 docker run --rm \
-    -p 8080:8080 \
-    -p 50000:50000 \
     -v `pwd`:/root/work \
     denden047/mpi4py \
     /bin/bash -c "${CMD}"
