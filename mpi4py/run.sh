100923ca7efc6f5b6999ccab733d163f89591d5b
Unknown <sh.mn.nat@gmail.com>
2018-11-28 11:26:44 +0900

    Hello worldが動いた

--- mpi4py/run.sh
+++ mpi4py/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+CMD="python main.py"
+
 docker run --rm \
     -p 8080:8080 \
     -p 50000:50000 \
-    -v `pwd`/jenkins_home:/var/jenkins_home \
-    denden047/jenkins
+    -v `pwd`:/root/work \
+    denden047/mpi4py \
+    /bin/bash -c "${CMD}"
