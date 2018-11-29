e90f6cccd47844c9e35686615b63853013b6fecd
DenDen047 <sh.mn.nat@gmail.com>
2018-11-29 16:50:12 +0900

    とりあえず動いた

--- DNC/run.sh
+++ DNC/run.sh
@@ -4,6 +4,6 @@ CMD="python main.py"
 
 docker run -it --rm \
     -v `pwd`:/root/work \
-    -p 8888:8888 \
+    -p 8887:8888 \
     denden047/dnc \
-    /bin/bash -c "${CMD}"
+    # /bin/bash -c "${CMD}"
